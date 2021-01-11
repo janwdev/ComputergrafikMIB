ea0dfc6ab5b8b75a731b51ecbe8b0a122f7e72fa
janwdev <jannik.weisser@hs-furtwangen.de>
2021-01-11 16:10:50 +0100

    11

--- Tut11_AssetsPickingCompleted/Tut11_AssetsPicking.cs
+++ Tut11_AssetsPickingCompleted/Tut11_AssetsPicking.cs
@@ -19,18 +19,23 @@ namespace FuseeApp
     [FuseeApplication(Name = "Tut11_AssetsPicking", Description = "Yet another FUSEE App.")]
     public class Tut11_AssetsPicking : RenderCanvas
     {
-         private SceneContainer _scene;
+        private SceneContainer _scene;
         private SceneRendererForward _sceneRenderer;
         private ScenePicker _scenePicker;
-        private Transform _baseTransform;
-        private Transform _rightRearTransform;
+        private Transform _carTransform;
+        private Transform _wheelBackR;
+        private Transform _wheelBackL;
+        private Transform _wheelFrontR;
+        private Transform _wheelFrontL;
+        private Transform _gabelHalterung;
+        private Transform _gabelPlatte;
         private PickResult _currentPick;
         private float4 _oldColor;
-        
+
         SceneContainer CreateScene()
         {
             // Initialize transform components that need to be changed inside "RenderAFrame"
-            _baseTransform = new Transform
+            _carTransform = new Transform
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
@@ -47,7 +52,7 @@ namespace FuseeApp
                         Components = new List<SceneComponent>
                         {
                             // TRANSFROM COMPONENT
-                            _baseTransform,
+                            _carTransform,
 
                             // SHADER EFFECT COMPONENT
                             SimpleMeshes.MakeMaterial((float4) ColorUint.LightGrey),
@@ -67,9 +72,17 @@ namespace FuseeApp
         {
             RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
 
-            _scene = AssetStorage.Get<SceneContainer>("CubeCar.fus");
+            _scene = AssetStorage.Get<SceneContainer>("Gabelstabler.fus");
 
-            _rightRearTransform = _scene.Children.FindNodes(node => node.Name == "RightRearWheel")?.FirstOrDefault()?.GetTransform();
+            _carTransform = _scene.Children.FindNodes(node => node.Name == "Fahrzeug")?.FirstOrDefault()?.GetTransform();
+
+            _wheelBackR = _scene.Children.FindNodes(node => node.Name == "Rad_RH")?.FirstOrDefault()?.GetTransform();
+            _wheelBackL = _scene.Children.FindNodes(node => node.Name == "Rad_LH")?.FirstOrDefault()?.GetTransform();
+            _wheelFrontL = _scene.Children.FindNodes(node => node.Name == "Rad_LV")?.FirstOrDefault()?.GetTransform();
+            _wheelFrontR = _scene.Children.FindNodes(node => node.Name == "Rad_RV")?.FirstOrDefault()?.GetTransform();
+
+            _gabelHalterung = _scene.Children.FindNodes(node => node.Name == "Gabel")?.FirstOrDefault()?.GetTransform();
+            _gabelPlatte = _scene.Children.FindNodes(node => node.Name == "Gabel_Platte")?.FirstOrDefault()?.GetTransform();
 
             // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRendererForward(_scene);
@@ -77,18 +90,15 @@ namespace FuseeApp
         }
 
         // RenderAFrame is called once a frame
-        // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-             SetProjectionAndViewport();
-
-            _rightRearTransform.Rotation = new float3(M.MinAngle(TimeSinceStart), 0, 0);
+            SetProjectionAndViewport();
 
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Math.Atan(15.0 / 40.0));
+            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float)Math.Atan(15.0 / 40.0));
 
             if (Mouse.LeftButton)
             {
@@ -107,9 +117,67 @@ namespace FuseeApp
                     {
                         var ef = newPick.Node.GetComponent<SurfaceEffect>();
                         _oldColor = ef.SurfaceInput.Albedo;
-                        ef.SurfaceInput.Albedo = (float4) ColorUint.OrangeRed;
+                        ef.SurfaceInput.Albedo = (float4)ColorUint.OrangeRed;
                     }
                     _currentPick = newPick;
+                }
+            }
+
+            if (_currentPick != null)
+            {
+                if (_currentPick.Node.Name == "Gabel_Platte")
+                {
+                    if (Keyboard.GetKey(KeyCodes.Up))
+                    {
+                        plattformUp();
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Down))
+                    {
+                        plattformDown();
+                    }
+                }
+                if (_currentPick.Node.Name == "Gabel")
+                {
+                    if (Keyboard.GetKey(KeyCodes.Up))
+                    {
+                        rotHalterungUp();
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Down))
+                    {
+                        rotHalterungDown();
+                    }
+                }
+                Diagnostics.Debug(_currentPick.Node.Name);
+                if (_currentPick.Node.Name == "Rad_RH" || _currentPick.Node.Name == "Rad_LH" || _currentPick.Node.Name == "Rad_RV" || _currentPick.Node.Name == "Rad_LV")
+                {
+                    if (Keyboard.GetKey(KeyCodes.Left))
+                    {
+                        driveBackward();
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Right))
+                    {
+                        driveForward();
+                    }
+                }
+                if (_currentPick.Node.Name == "Fahrzeug")
+                {
+                    if (Keyboard.GetKey(KeyCodes.Up))
+                    {
+                        _carTransform.Rotation = new float3(0, _carTransform.Rotation.y + 1.5f * Time.DeltaTime, 0);
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Down))
+                    {
+                        _carTransform.Rotation = new float3(0, _carTransform.Rotation.y - 1.5f * Time.DeltaTime, 0);
+                    }
+
+                    if (Keyboard.GetKey(KeyCodes.Left))
+                    {
+                        driveBackward();
+                    }
+                    else if (Keyboard.GetKey(KeyCodes.Right))
+                    {
+                        driveForward();
+                    }
                 }
             }
 
@@ -118,6 +186,55 @@ namespace FuseeApp
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
             Present();
+        }
+
+        public void plattformUp()
+        {
+            if (_gabelPlatte.Translation.y < 2)
+            {
+                _gabelPlatte.Translation.y = _gabelPlatte.Translation.y + 1.5f * Time.DeltaTime;
+            }
+        }
+        public void plattformDown()
+        {
+            if (_gabelPlatte.Translation.y > -2)
+            {
+                _gabelPlatte.Translation.y = _gabelPlatte.Translation.y - 1.5f * Time.DeltaTime;
+            }
+        }
+
+        public void rotHalterungUp()
+        {
+            if (_gabelHalterung.Rotation.z < Math.PI / 6)
+            {
+                _gabelHalterung.Rotation.z = _gabelHalterung.Rotation.z + 1.5f * Time.DeltaTime;
+            }
+        }
+
+        public void rotHalterungDown()
+        {
+            if (_gabelHalterung.Rotation.z > -Math.PI / 6)
+            {
+                _gabelHalterung.Rotation.z = _gabelHalterung.Rotation.z - 1.5f * Time.DeltaTime;
+            }
+        }
+
+        public void driveForward()
+        {
+            _wheelBackR.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+            _wheelBackL.Rotation = new float3((float)Math.PI, M.MinAngle(TimeSinceStart), 0);
+            _wheelFrontL.Rotation = _wheelBackL.Rotation;
+            _wheelFrontR.Rotation = _wheelBackR.Rotation;
+            _carTransform.Translation.x = _carTransform.Translation.x + 1.5f * Time.DeltaTime;
+        }
+
+        public void driveBackward()
+        {
+            _wheelBackR.Rotation = new float3(0, -M.MinAngle(TimeSinceStart), 0);
+            _wheelBackL.Rotation = new float3((float)Math.PI, -M.MinAngle(TimeSinceStart), 0);
+            _wheelFrontL.Rotation = _wheelBackL.Rotation;
+            _wheelFrontR.Rotation = _wheelBackR.Rotation;
+            _carTransform.Translation.x = _carTransform.Translation.x - 1.5f * Time.DeltaTime;
         }
 
         public void SetProjectionAndViewport()
@@ -133,6 +250,6 @@ namespace FuseeApp
             // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
             var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
             RC.Projection = projection;
-        }                
+        }
     }
 }
